a0e7efead4ab9d2148caf3b8ea9417240b50fa76
myxzlpltk <myxzlpltk@gmail.com>
2022-06-22 13:43:11 +0700

    profil validator

--- lib/modules/features/profile/view/components/email_bottom_sheet.dart
+++ lib/modules/features/profile/view/components/email_bottom_sheet.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:form_validator/form_validator.dart';
 import 'package:get/get.dart';
+import 'package:java_code_app/configs/localizations/localization.dart';
 import 'package:java_code_app/configs/themes/colors.dart';
 import 'package:java_code_app/modules/features/menu/view/components/holder_bottom_sheet.dart';
 
@@ -15,6 +18,7 @@ class EmailBottomSheet extends StatefulWidget {
 
 class _EmailBottomSheetState extends State<EmailBottomSheet> {
   late final TextEditingController controller;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -36,24 +40,30 @@ class _EmailBottomSheetState extends State<EmailBottomSheet> {
           Row(
             children: [
               Expanded(
-                child: TextField(
-                  controller: controller,
-                  style: Get.textTheme.bodySmall,
-                  decoration: InputDecoration(
-                    hintText: 'Email'.tr,
-                    hintStyle: Get.textTheme.bodySmall,
-                    enabledBorder: const UnderlineInputBorder(
-                      borderSide:
-                          BorderSide(color: AppColor.blueColor, width: 2),
+                child: Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    controller: controller,
+                    style: Get.textTheme.bodySmall,
+                    decoration: InputDecoration(
+                      hintText: 'Email'.tr,
+                      hintStyle: Get.textTheme.bodySmall,
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.blueColor, width: 2),
+                      ),
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.blueColor, width: 2),
+                      ),
                     ),
-                    focusedBorder: const UnderlineInputBorder(
-                      borderSide:
-                          BorderSide(color: AppColor.blueColor, width: 2),
-                    ),
+                    maxLength: 100,
+                    keyboardType: TextInputType.emailAddress,
+                    autofocus: true,
+                    validator: ValidationBuilder(
+                      localeName: Localization.currentLocale.languageCode,
+                    ).email().required().build(),
                   ),
-                  maxLength: 100,
-                  keyboardType: TextInputType.emailAddress,
-                  autofocus: true,
                 ),
               ),
               12.verticalSpacingRadius,
@@ -61,7 +71,11 @@ class _EmailBottomSheetState extends State<EmailBottomSheet> {
                 icon: const Icon(Icons.check_circle),
                 splashRadius: 20.r,
                 color: AppColor.blueColor,
-                onPressed: () => Get.back(result: controller.text),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Get.back(result: controller.text);
+                  }
+                },
               ),
             ],
           ),
--- lib/modules/features/profile/view/components/phone_bottom_sheet.dart
+++ lib/modules/features/profile/view/components/phone_bottom_sheet.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:form_validator/form_validator.dart';
 import 'package:get/get.dart';
+import 'package:java_code_app/configs/localizations/localization.dart';
 import 'package:java_code_app/configs/themes/colors.dart';
 import 'package:java_code_app/modules/features/menu/view/components/holder_bottom_sheet.dart';
 
@@ -15,6 +18,7 @@ class PhoneBottomSheet extends StatefulWidget {
 
 class _PhoneBottomSheetState extends State<PhoneBottomSheet> {
   late final TextEditingController controller;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -36,24 +40,33 @@ class _PhoneBottomSheetState extends State<PhoneBottomSheet> {
           Row(
             children: [
               Expanded(
-                child: TextField(
-                  controller: controller,
-                  style: Get.textTheme.bodySmall,
-                  decoration: InputDecoration(
-                    hintText: 'Phone number'.tr,
-                    hintStyle: Get.textTheme.bodySmall,
-                    enabledBorder: const UnderlineInputBorder(
-                      borderSide:
-                          BorderSide(color: AppColor.blueColor, width: 2),
+                child: Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    controller: controller,
+                    style: Get.textTheme.bodySmall,
+                    decoration: InputDecoration(
+                      hintText: 'Phone number'.tr,
+                      hintStyle: Get.textTheme.bodySmall,
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.blueColor, width: 2),
+                      ),
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.blueColor, width: 2),
+                      ),
                     ),
-                    focusedBorder: const UnderlineInputBorder(
-                      borderSide:
-                          BorderSide(color: AppColor.blueColor, width: 2),
-                    ),
+                    maxLength: 100,
+                    keyboardType: TextInputType.phone,
+                    autofocus: true,
+                    validator: ValidationBuilder(
+                      localeName: Localization.currentLocale.languageCode,
+                    ).phone().required().build(),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
                   ),
-                  maxLength: 100,
-                  keyboardType: TextInputType.phone,
-                  autofocus: true,
                 ),
               ),
               12.verticalSpacingRadius,
@@ -61,7 +74,11 @@ class _PhoneBottomSheetState extends State<PhoneBottomSheet> {
                 icon: const Icon(Icons.check_circle),
                 splashRadius: 20.r,
                 color: AppColor.blueColor,
-                onPressed: () => Get.back(result: controller.text),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Get.back(result: controller.text);
+                  }
+                },
               ),
             ],
           ),
