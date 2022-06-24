3cc107a91306fe658ed5fa5fdca1154863bcb9ff
myxzlpltk <myxzlpltk@gmail.com>
2022-06-24 08:43:55 +0700

    Revert "order card center vertically"
    This reverts commit c25c5bbe76d660171ec1d0e3995443d9ed17d540.

--- lib/modules/features/order/view/components/order_card.dart
+++ lib/modules/features/order/view/components/order_card.dart
@@ -43,6 +43,7 @@ class OrderCard extends StatelessWidget {
           ],
         ),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Flexible(
               flex: 3,
